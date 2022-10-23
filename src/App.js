3cb3d803533d7d90e268cba14c479f81b97b01c5
Ron <wondelrosario@gmail.com>
2022-10-23 19:49:37 +1400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="text-center text-white">
+      <h1 className="text-6xl font-bold">
+        BASIC SETUP 
+      </h1>
+      <h3 className="text-2xl py-4">
+        REACT & TAILWIND
+      </h3>
+    </div>
+  );
+}
+
+export default App;
